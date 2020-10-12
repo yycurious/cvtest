07ed3bde8aa9360e4a64b98caad9601345127da1
yy_curious <yy_curious@yahoo.com>
2020-10-12 21:57:53 +0300

    Blank user interface

---
+++ CVTest/CVTest/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using CVTest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CVTest
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //Log.Instance.Debug("We're going to start ShellViewModel");
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
